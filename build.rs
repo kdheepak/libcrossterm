df6f95254fde1e5dff90097e399e481bb765dba4
Dheepak Krishnamurthy <me@kdheepak.com>
2023-07-20 21:41:30 -0400

    feat: Make header file name crossterm ✨

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
   let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
   let bindings = cbindgen::generate(&crate_dir).expect("Failed to generate bindings");
-  bindings.write_to_file(format!("{}/include/libcrossterm.h", crate_dir));
+  bindings.write_to_file(format!("{}/include/crossterm.h", crate_dir));
 }
