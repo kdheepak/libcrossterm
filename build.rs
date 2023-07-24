8a56b396a90730609cf82d9de38c36664ede31bd
Dheepak Krishnamurthy <me@kdheepak.com>
2023-07-23 23:19:39 -0400

    feat: don't run build.rs on docs.rs ✨

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 fn main() {
+  // Do not update files on docsrs
+  if std::env::var("DOCS_RS").is_ok() {
+    return;
+  }
   let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap_or(".".to_string());
   if let Ok(bindings) = cbindgen::generate(&crate_dir) {
     bindings.write_to_file(format!("{}/include/crossterm.h", crate_dir));
